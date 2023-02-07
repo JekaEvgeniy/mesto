2bdc0ad9ad5aa7962125969506a171fe9cfe17a6
JekaEvgeniy <pruven@yandex.ru>
2023-02-07 04:07:44 +0300

    script: listener closed popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 
 (function () {
 	let popupBtn = document.querySelectorAll('.js-toggle-popup');
+	let popupBtnClose = document.querySelectorAll('.js-popup-close');
 	let popupToggleClass = 'popup_is-opened';
 
 	for (let i = 0; i < popupBtn.length; i++) {
@@ -13,6 +14,16 @@
 				if (popup) {
 					popup.classList.add(popupToggleClass);
 				}
+			}
+		});
+	}
+
+	for (let i = 0; i < popupBtnClose.length; i++) {
+		let el = popupBtnClose[i];
+		el.addEventListener('click', function () {
+			let activePopup = document.querySelector('.popup_is-opened');
+			if (activePopup ){
+				activePopup.classList.remove(popupToggleClass);
 			}
 		});
 	}
