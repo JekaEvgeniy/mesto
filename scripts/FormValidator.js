5e8888d0798d5390cd8bb3dcc793c57d7d3d935e
JekaEvgeniy <pruven@yandex.ru>
2023-04-07 01:41:14 +0300

    edit: попытка #2. старт валидации формы

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -10,10 +10,73 @@ export default class FormValidator {
 		this._inactiveButtonClass = classes.inactiveButtonClass;
 		this._inputErrorClass = classes.inputErrorClass;
 		this._errorClass = classes.errorClass;
+
+		this._inputs = Array.from( this._form.querySelectorAll(classes.inputSelector));
+		this._btnSubmit = this._form.querySelector(classes.submitButtonSelector);
 	}
 
+	_setEventListeners = () => {
+		const form = this._form;
+		const classes = this._classes;
+		const inputs = this._inputs;
+
+		inputs.forEach((inputElement) => {
+
+			inputElement.addEventListener('input', (e) => {
+				// this._toggleInputState(inputElement);
+				this._toggleBtnState();
+			});
+
+		});
+
+		// делаем так, чтобы при загрузке страницы кнопка была неактивна (disabled)
+		this._toggleBtnState();
+	};
+
+	// _toggleInputState = (inputElement) => {
+	// 	const isValid = inputElement.validity.valid;
+	// 	const inputElementId = inputElement.id;
+	// 	const errorElement = document.querySelector(`#${inputElementId}-error`);
+
+	// 	if (!isValid) {
+	// 		this._addStateInputError(inputElement);
+
+	// 		this._showMessageInputError(errorElement, inputElement);
+	// 	} else {
+	// 		this._removeStateInputError(inputElement);
+
+	// 		this._hideMessageInputError(errorElement, inputElement);
+	// 	}
+	// }
+
+	_toggleBtnState = () => {
+		const checkValid = this._inputs.every(el => {
+			return el.validity.valid;
+		});
+
+		console.log(`checkValid = ${checkValid}`)
+		if (!checkValid) {
+			this._disableButton();
+		} else {
+			this._enableButton();
+		}
+	};
+
+	_disableButton = () => {
+		const classes = this._classes;
+		this._btnSubmit.classList.add(classes.inactiveButtonClass);
+		this._btnSubmit.disabled = true;
+	}
+
+	_enableButton = () => {
+		const classes = this._classes;
+		this._btnSubmit.classList.remove(classes.inactiveButtonClass);
+		this._btnSubmit.disabled = false;
+	}
+
+
+
 	enableValidation(){
-		console.log(this);
-		// this._setEventListeners();
+		this._setEventListeners();
 	}
 }
